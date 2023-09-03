2378e8fb529939ac96aa38a6a034a59a634f081b
IrinaGM <irina.golionko@gmail.com>
2023-09-03 12:03:04 +0000

    setup initial users controller, routes &  signup page

--- config/routes.rb
+++ config/routes.rb
@@ -20,6 +20,8 @@ Rails.application.routes.draw do
     resources :categories, except: [:edit, :update, :show, :destroy]
   end
 
+  resources :users, only: [:new, :create]
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
